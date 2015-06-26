2d23e5af0efac6f92c087467c8fd220fd19fbffa
chrissie and ray <code@braintreepayments.com>
2015-06-26 22:37:20 +0000

    Fix argument to GithubGateway test

--- spec/github_gateway_spec.rb
+++ spec/github_gateway_spec.rb
@@ -3,14 +3,17 @@ require 'github_gateway'
 
 describe GithubGateway do
   before(:all) do
-    @gateway = GithubGateway.new( {
-      :parent_repo => "chrissiedeist"
-    })
+    @gateway = GithubGateway.new("chrissiedeist")
+    @repo = "django_blog"
   end
 
   it "gets all issues for a particular repo" do
-    repo = "django_blog"
-    expect(@gateway.issues_for(repo).length).to eq(1)
+    expect(@gateway.issues_for(@repo).length).to eq(1)
   end
 
+  it "can access the title and body of the issue" do
+    issue = @gateway.issues_for(@repo).first
+    expect(issue.title).to eq("Test")
+    expect(issue.body).to eq("This is a test.")
+  end
 end
